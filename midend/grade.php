2c5a3fda8527352d415f390cbdb2f09feb8d221b
marisa <mlsig95@gmail.com>
2019-11-12 20:17:34 -0500

    better

--- midend/grade.php
+++ midend/grade.php
@@ -1,44 +1,16 @@
 <?php
 //get data from gc
-//$j = $_POST['json'];
-
-$j = '[{
-    "qid": "qid0",
-    "title": "returnDouble",
-    "sol": "def returnDouble(num):\n\treturn num*2",
-    "io": [{
-        "in": "2",
-        "out": "4"
-    }, {
-        "in": "3",
-        "out": "6"
-    }],
-    "rubric": "10"
-}, {
-    "qid": "qid1",
-    "title": "returnHalf",
-    "sol": "def returnHalf(num):\n\treturn num*.5",
-    "io": [{
-        "in": "4",
-        "out": "2"
-    }],
-    "rubric": "15"
-}]';
-
+$j = $_POST['json'];
 //make data list
 $data = json_decode($j);
-
 //track what a perfct score is
 $perf = 0;
-
 //track exam score
 $g = 0;
-
 //array for the new question data
 $qs = [];
 
 foreach ($data as &$q) {
-
     //get info
     $qid = $q->qid;
     $t = $q->title;
@@ -46,10 +18,8 @@ foreach ($data as &$q) {
     $io = $q->io;
     //starting perfect, subtracting as we go
     $r = $q->rubric;
-    
     //track perfect score
     $perf = $perf + $r;
-    
     //array of comments
     $c = [];
     
@@ -57,7 +27,7 @@ foreach ($data as &$q) {
     $func = strstr($s, $t);
     $pos = strpos($s, $t);
     $fun = gettype($func);
-    if($fun != "string" || $pos != 4){
+    if($fun != "string" && $pos != 4){
         //find fucntion name
         $fi = explode("(",$s);
         $f = $fi[0];
@@ -74,29 +44,30 @@ foreach ($data as &$q) {
     $typ = gettype($col);
     $fi = explode(")",$s);
     $f = strlen($fi[0]) + 1;
-    
-    if($typ != "string" || $pos != $f){ //NEED DO
-        //add colon at pos
+    if($typ != "string" && $pos != $f){ //NEED DO
         /*
-        $fi = explode("(",$s);
-        $f = $fi[0];
+        $ree = $fi[0];
+        echo $ree;
         $de = "def " . $t;
         //fix their fucntion name
         $s = str_replace($f,$de,$s);
         */
         $r = $r - 3;
-        array_push($c,"Minus 3 for missing c");
+        array_push($c,"Minus 3 for missing colon");
     }
-
+    
+    //check hardcode
+    
     //add and run each io calls
     $index = 0; 
-    foreach ($i as &$tests){
-        $in = $i["in"];
-        $out = $i["out"];
+    //why loop no work?
+    foreach ($io as &$i){
+        $in = $i->in;
+        $out = $i->out;
         $call = "print(" . $t . "(" . $in . "))";
         $code = $s . "\n{$call}";
-        $c = "echo \"{$code}\" | python -";
-        $output = shell_exec($c);
+        $co = "echo \"{$code}\" | python -";
+        $output = shell_exec($co);
         $o = trim(preg_replace('/\s+/', ' ', $output));
         if($out != $o){
             $r = $r - 10;
@@ -108,6 +79,7 @@ foreach ($data as &$q) {
         $index++;
     }
     
+    //grade update
     $g = $g + $r;
     
     //push to qs array
